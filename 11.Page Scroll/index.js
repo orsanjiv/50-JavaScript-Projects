7a2e63f001dc4e988204ca0deee67f860b208d24
SANJIV GUPTA <90419613+orsanjiv@users.noreply.github.com>
2024-05-27 18:11:00 +0530

    Page Scroll Functionality Achieved

--- 11.Page Scroll/index.js
+++ 11.Page Scroll/index.js
@@ -1,2 +1,6 @@
-const heading = document.getElementById('heading')
-console.log(heading)
+const scrollbar = document.getElementById('scrollbar')  
+
+window.addEventListener('scroll', () => {
+    let percentage = (window.scrollY/(document.body.scrollHeight - window.innerHeight)) * 100
+    scrollbar.style.width = percentage + '%'
+})
